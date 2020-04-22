7e1f5d75ef8d6cb5e7994703702795210ed1ddba
BudhiChristian <cjbudhi95@gmail.com>
2020-04-22 16:45:54 -0400

    Initial Commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# What Am I?
+
+This is a simple Node application utilizing Express to host multiple static websites.
---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+
+const port = 3000;
+const host = '0.0.0.0';
+const angularResources = [
+    "ner-annotation-suite"
+];
+
+
+angularResources.forEach(resource => {
+    // Build URI under "projects" route
+    let uri = '/projects/' + resource;
+    // host angular application as static page
+    app.use(uri, express.static(__dirname + uri));
+    // direct any sub routes to this application
+    app.get(uri + '/*', (req, res, next) => {
+        res.sendFile(path.join(__dirname, uri, 'index.html'))
+    })
+});
+app.use('/', express.static(__dirname + '/portfolio-site'));
+app.get('/*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '/portfolio-site', 'index.html'))
+})
+// app.get('/', (req, res, next) => {
+//     res.send('hello world')
+// });
+
+// app.get('/*', (req, res, next) => {
+//     res.redirect('/')
+// })
+app.listen(port, host, () => {
+    console.log(__dirname)
+    console.log(`Project Running on port ${port}`);
+});
