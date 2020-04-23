ab2437ebca8e1693b8891a57915c5349469c89a7
BudhiChristian <cjbudhi95@gmail.com>
2020-04-23 11:25:47 -0400

    reverting port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path')
 const app = express();
 
-const port = 80;
+const port = 3000;
 const host = '0.0.0.0';
 const angularResources = [
     "ner-annotation-suite"
